730e6a9d42bc8a32db5f57a13416b24f6ab769e1
Didisimmons <onyema.onyejekwe@gmail.com>
2022-03-11 14:38:12 +0000

    create webhook for shopping bag

--- checkout/views.py
+++ checkout/views.py
@@ -49,9 +49,12 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            if order_form.is_valid():
-                order = order_form.save()
+        if order_form.is_valid():  
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             # iterating through the bag items to create each line item
             for item_id, item_data in bag.items():
                 try:
