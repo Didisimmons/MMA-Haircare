85f395964aa07e9521ad90063fdcf7ed54242960
Didisimmons <onyema.onyejekwe@gmail.com>
2022-03-12 22:26:55 +0000

    add product form view, url and templates

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 from django.db.models.functions import Lower
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -68,3 +69,29 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only Admin has access.')
+        return redirect(reverse('home'))
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        # if form valid
+        if form.is_valid():
+            product = form.save()  # To store the product
+            messages.success(request, 'Successfully added product!')
+            # update the redirect url to that of the product added
+            return redirect(reverse('product_detail', args=[product.id]))  # redirect to the products detail page
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')  # if any errors alert user to check form
+    else:
+        form = ProductForm()  # this empty form instantiation ensures it doesn't wipe out the form errors.
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
