a734c0f435ecf80c0e7438e4520129ba82f41950
Didisimmons <onyema.onyejekwe@gmail.com>
2022-03-12 15:42:25 +0000

    added profile form,views and updated template

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -8,12 +10,22 @@ from .forms import UserProfileForm
 @login_required
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    form = UserProfileForm(instance=profile)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Address updated successfully')
+        else:
+            messages.error(request, 'Address not updated, ensure fields are filled correctly.')
+    else:
+        form = UserProfileForm(instance=profile)
+ 
     orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
     return render(request, template, context)
