d031df8bada4a084713ec7ddf44a489a59b3c5f9
Didisimmons <onyema.onyejekwe@gmail.com>
2022-03-13 16:20:06 +0000

    add delete functionality

--- products/views.py
+++ products/views.py
@@ -122,3 +122,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only admin has access.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
